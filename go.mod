139d5499d93ab13d970401e61df27536321ccc1f
Rizki Prasetia <rizkiprasetia98@gmail.com>
2023-10-25 04:38:53 +0700

    [Update]config

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module test_mnc
 
 go 1.20
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+)
---
+++ manager/usecase_manager.go
@@ -0,0 +1,26 @@
+package manager
+
+type UseCaseManager interface {
+	// AuthUseCase() usecase.AuthUseCase
+	// UserUseCase() usecase.UserUseCase
+}
+
+type useCaseManager struct {
+	repoManager RepoManager
+}
+
+// // AuthUseCase implements UseCaseManager.
+// func (u *useCaseManager) AuthUseCase() usecase.AuthUseCase {
+// 	return usecase.NewAuthUseCase(u.repoManager.UserRepo())
+// }
+
+// // UserUseCase implements UseCaseManager.
+// func (u *useCaseManager) UserUseCase() usecase.UserUseCase {
+// 	return usecase.NewUserUseCase(u.repoManager.UserRepo())
+// }
+
+func NewUseCaseManager(repoManager RepoManager) UseCaseManager {
+	return &useCaseManager{
+		repoManager: repoManager,
+	}
+}
